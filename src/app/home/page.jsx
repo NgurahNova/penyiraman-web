b766b2807969176e10daa7ce1221f3b5dda70b1f
AdiPalgunaa <palgunaa7@gmail.com>
2025-06-20 16:44:34 +0800

    updatenutrisi

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -39,6 +39,8 @@ const Page = () => {
   const [temperaturetds, setTemperatureTds] = useState(0);
   const [threshold, setThreshold] = useState("");
   const inputRef = useRef(null);
+  const tank1Status = getTankLevelStatus(distance1);
+  const tank2Status = getTankLevelStatus(distance2);
 
   useEffect(() => {
     const dataRef = ref(database, "realtime_data");
@@ -125,9 +127,6 @@ const Page = () => {
         };
       }
     };
-
-    const tank1Status = getTankLevelStatus(distance1);
-    const tank2Status = getTankLevelStatus(distance2);
 
   // Function to determine moisture level color
   const getMoistureColor = (value) => {
